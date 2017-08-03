96d9f52de2219f536ce4edaf3cd74a791a1e5e1b
cogitom <tomek.wasik1@gmail.com>
2017-08-03 22:31:36 +0200

    First Commit

---
+++ CardsDeck.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumKart
+{
+    public class CardsDeck :IQueueCards
+    {
+        private Queue <Card> cards;
+        
+        public CardsDeck()
+        {
+            cards = new Queue<Card>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 2; j <= 14; j++)
+                {
+                    cards.Enqueue(new Card((Suits)i, (Values)j));
+                }
+            }
+        }
+
+        public CardsDeck(Queue<Card> cardStack)
+        {
+            cards = new Queue<Card>(cardStack);
+        }
+
+        public Card PeekCard
+        {
+            get
+            {
+                return cards.Peek();
+            }
+         
+        }
+        
+        public int NumberOfCards
+        {
+            get
+            {
+                return cards.Count;
+            }
+        }
+
+        public void CollectCard(Card card)
+        {
+            cards.Enqueue(card);
+        }
+
+        public Card PutCard()
+        {
+            return cards.Dequeue();
+        }
+
+        public void Shuffle(int numberOfShuffles)
+        {
+            Random random = new Random();
+            List<Card> newCardDeck = cards.ToList();
+
+            for (int c = 0; c < numberOfShuffles; c++)
+            {
+                for (int i = 0; i <= cards.Count - 1; i++)
+                {
+                    int r = random.Next(i, cards.Count - 1);
+
+                    Card buffer = newCardDeck[r];
+
+                    newCardDeck[r] = newCardDeck[i];
+                    newCardDeck[i] = buffer;
+
+                }
+
+                int last_random = random.Next(0, cards.Count - 1);
+                Card lastCard = newCardDeck.Last();
+
+                newCardDeck[cards.Count - 1] = newCardDeck[last_random];
+                newCardDeck[last_random] = lastCard;
+
+                cards.Clear();
+
+
+                foreach (Card card in newCardDeck)
+                {
+                    cards.Enqueue(card);
+                }
+                
+            }
+            
+          
+
+        }
+   
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace enumKart
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        CardsDeck cardPile;
+        List<CardOnStack> cardsOnStack;
+        List <Player> players;
+        Player Tom, Bob, Lily;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            random = new Random();
+            cardPile = new CardsDeck();
+            Tom = new Player("Tom",TomSurfaceCardTextBox, TomNumberOfCards);
+            Bob = new Player("Bob", bobSurfaceCardTextBox, BobNumberOfCards);
+            Lily = new Player("Lily", LilySurfaceCardTextBox, LilyNumberOfCards);
+
+            players = new List<Player>() { Tom, Bob, Lily };
+
+        }
+        
+        private void nextTourButton_Click(object sender, EventArgs e)
+        {
+            TomSurfaceCardTextBox.BackColor = DefaultBackColor;
+            bobSurfaceCardTextBox.BackColor = DefaultBackColor;
+            LilySurfaceCardTextBox.BackColor = DefaultBackColor;
+
+            foreach (Player player in players)
+            {
+                player.UpdateTextBoxes();
+            }
+
+            Player winner = nextTour();
+
+            winner.textBox.BackColor = Color.Green;
+
+            for(int i =0; i<players.Le)
+            {
+                if (player == winner)
+                {
+                    foreach (Card card in cardsOnStack)
+                    {
+                        player.CollectCard(card);
+                    }
+                }
+                if (player.NumberOfCards <= 0)
+                {
+                    players.Remove(player);
+                }
+            }
+            cardsOnStack.Clear();
+        }
+
+     
+
+        private void newDealButton_Click(object sender, EventArgs e)
+        {
+            cardPile.Shuffle(10);
+            int position=0;
+            int playerPosition;
+
+            while(cardPile.NumberOfCards != 0)
+            {
+                playerPosition = position % 3;
+                players[playerPosition].CollectCard(cardPile.PutCard());
+                position++;
+            }            
+        }
+
+        private Player nextTour()
+        {
+            Player winner = players[0];
+
+            cardsOnStack = new List<CardOnStack>();
+
+            foreach (Player player in players)
+            {
+                if (player.NumberOfCards > 0)
+                {
+                    cardsOnStack.Add(new CardOnStack(player.PutCard(), player));
+                }
+                else
+                {
+                    players.Remove(player);
+                }
+            }
+
+            cardsOnStack.Sort();
+
+            return cardsOnStack.Last().Owner;
+        }
+       
+       
+    }
+}
